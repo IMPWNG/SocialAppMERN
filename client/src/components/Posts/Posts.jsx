f9dddf8f53eecada883f651d02196cc36511e3ce
IMPWNG <matisse.ach+github.com>
2022-05-26 10:45:39 -0400

    addClient folder

---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+
+export default function Posts() {
+  const classes = useStyles();
+  return (
+    <>
+      <h1>Postddvdvdfvddds</h1>
+      <Post />
+      <Post />
+    </>
+  );
+  
+  
+  
+}
