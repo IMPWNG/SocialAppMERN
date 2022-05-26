2369961e19eb5220296678aa1ba66274d60a407a
IMPWNG <matisse.ach+github.com>
2022-05-26 11:42:36 -0400

    redux logic ✅

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ config({ path: config.env });
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.NODE_APP_MONGODB_CONNECT;
 const PORT = process.env.PORT || 5000;
